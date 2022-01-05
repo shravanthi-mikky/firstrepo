e4d6bcee8d165d2be5bb9464662b053a29af8978
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-05 19:45:16 +0530

    uc1 new commit

--- firstOne.sh
+++ firstOne.sh
@@ -1,3 +1,11 @@
 echo "Welcome to Employee Wage Computation program on Master Branch"
 echo "Check Employee is Present or Absent"
-echo "-Use ((RANDOM)) for Attendance Check"
+isPresent=1
+randomValue=$(($RANDOM%2))
+if [ $isPresent -eq $randomValue ]
+then
+	echo "Employee is Present. "
+else
+	echo "Employee is Absent. "
+fi
+
